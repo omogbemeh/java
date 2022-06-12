d35362065f1ce91284980b4c1bfef3a76ffca911
Praise Omogbemeh <onosithena@gmail.com>
2022-06-11 23:34:45 -0400

    Learning Regex

---
+++ src/Regex/Regex.java
@@ -0,0 +1,9 @@
+package Regex;
+
+public class Regex {
+    public static void main(String[] args) {
+        String str = "Cat";
+        String regex = "\\wat";
+        System.out.println(str.matches(regex));
+    }
+}
