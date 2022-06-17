11b39ebd477e5248ab962eca07a4c7fbe0776c03
Praise Omogbemeh <onosithena@gmail.com>
2022-06-17 12:44:51 -0400

    Created a compound interest calculator using BigDecimal.

--- src/learningNumbers/Numbers.java
+++ src/learningNumbers/Numbers.java
@@ -1,10 +1,14 @@
 package learningNumbers;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 import java.util.Random;
 
 public class Numbers {
     public static void main(String[] args) {
-        System.out.println(calcCentrifugalForce(3.0, 0.2, 0.8));
+//        System.out.println(calcCentrifugalForce(3.0, 0.2, 0.8));
+        System.out.println(calcCompoundInterest("10000", ".08", "10", "1000"));
     }
 
     /**
@@ -36,5 +40,25 @@ public class Numbers {
 
     public static double calcCentripetalAcceleration(double pathVelocity, double radius) {
         return (Math.pow(pathVelocity, 2)) / radius;
+    }
+
+    /**
+     * Calculates compound interest using P(1 + r)^y + c[ ((1 + r)^y- 1)/r]
+     * @param p principal or initial capital
+     * @param r interest rate
+     * @param con annual contribution
+     * @param y number of years.
+     * @return
+     */
+    public static BigDecimal calcCompoundInterest(String p, String r, String y, String con) {
+        BigDecimal principal = new BigDecimal(p);
+        BigDecimal rate = new BigDecimal(r);
+        BigDecimal contribution = new BigDecimal(con);
+        BigDecimal year = new BigDecimal(y);
+        BigDecimal common = (rate.add(BigDecimal.ONE)).pow(year.intValue());
+        BigDecimal firstPart = principal.multiply(common);
+        BigDecimal secondPart = (common.subtract(BigDecimal.ONE)).divide(rate);
+        BigDecimal finalSecondPart = contribution.multiply(secondPart);
+        return firstPart.add(finalSecondPart).round(new MathContext(5, RoundingMode.HALF_UP));
     }
 }
